80266f13d53807a993c317bd149208e32e99e91d
Christopher Curley <clevercurley@gmail.com>
2023-08-20 17:52:22 -0400

    adds interface, repository, controller for image upload functionality

---
+++ Blog/Controllers/ImagesController.cs
@@ -0,0 +1,33 @@
+﻿using Blog.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadAsync(IFormFile file)
+        {
+            // call repository
+            var imageUrl = await imageRepository.UploadAsync(file);
+
+            if (imageUrl == null)
+            {
+                return Problem("Image upload was NOT successful.", null, (int)HttpStatusCode.InternalServerError);
+            }
+
+            return new JsonResult(new { link = imageUrl });
+        }
+    }
+}
